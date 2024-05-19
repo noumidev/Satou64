1d0f1ca3c4990263fa973f94b6c720c7d7086bdc
Nomi <nomiwen@outlook.de>
2024-05-19 05:26:02 +0200

    Add Status and Config regs

--- src/hw/cpu/cop0.cpp
+++ src/hw/cpu/cop0.cpp
@@ -15,14 +15,33 @@
 
 namespace hw::cpu::cop0 {
 
+constexpr u32 CONFIG_DEFAULT = 0x6E460;
+
 // COP0 registers
 namespace Register {
     enum : u32 {
         Count = 9,
         Compare = 11,
+        Status = 12,
         Cause = 13,
+        Config = 16,
     };
 };
+
+// Register write masks
+namespace WriteMask {
+    enum : u32 {
+        Config = 0xF00800F,
+    };
+}
+
+namespace CPUMode {
+    enum : u32 {
+        Kernel = 0,
+        Supervisor = 1,
+        User = 2,
+    };
+}
 
 // Cause register
 union Cause {
@@ -39,10 +58,55 @@ union Cause {
     };
 };
 
+// Config register
+union Config {
+    u32 raw;
+    struct {
+        u32 coherencyAlgorithm : 3;
+        u32 cu : 1;
+        u32 : 11;
+        u32 bigEndian : 1;
+        u32 : 8;
+        u32 dataPattern : 4;
+        u32 frequencyRatio : 3;
+        u32 : 1;
+    };
+};
+
+// Status register
+union Status {
+    u32 raw;
+    struct {
+        u32 interruptEnable : 1;
+        u32 exceptionLevel : 1;
+        u32 errorLevel : 1;
+        u32 mode : 2;
+        u32 ux : 1;
+        u32 sx : 1;
+        u32 kx : 1;
+        u32 interruptMask : 8;
+        u32 de : 1;
+        u32 ce : 1;
+        u32 condition : 1;
+        u32 : 1;
+        u32 softReset : 1;
+        u32 tlbShutdown : 1;
+        u32 bootExceptionVectors : 1;
+        u32 : 0;
+        u32 instructionTraceEnable : 1;
+        u32 reverseEndian : 1;
+        u32 fr : 1;
+        u32 lowPower : 1;
+        u32 coprocessorUsable : 4;
+    };
+};
+
 struct Registers {
     u32 count;
     u32 compare;
+    Status status;
     Cause cause;
+    Config config;
 };
 
 Registers regs;
@@ -53,6 +117,11 @@ void deinit() {}
 
 void reset() {
     std::memset(&regs, 0, sizeof(Registers));
+
+    regs.status.mode = CPUMode::Kernel;
+    regs.status.bootExceptionVectors = 1;
+
+    regs.config.raw = CONFIG_DEFAULT;
 }
 
 template<>
@@ -102,8 +171,14 @@ void set(const u32 idx, const u32 data) {
         case Register::Compare:
             regs.compare = data;
             break;
+        case Register::Status:
+            regs.status.raw = data;
+            break;
         case Register::Cause:
-            regs.cause.raw = data;
+            PLOG_WARNING << "Cause write (data = " << std::hex << data << ")";
+            break;
+        case Register::Config:
+            regs.config.raw = (data & WriteMask::Config) | (regs.config.raw & ~WriteMask::Config);
             break;
         default:
             PLOG_FATAL << "Unrecognized set32 register " << idx << " (data = " << std::hex << data << ")";
